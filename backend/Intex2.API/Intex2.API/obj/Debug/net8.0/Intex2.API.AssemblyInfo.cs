24323b659ba0cb4486765f8d5a8c752708942963
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-04-07 21:22:44 -0600

    merged

--- backend/Intex2.API/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
+++ backend/Intex2.API/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282260eb57d164f28027a5f482e4cef22df6b654")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebfe0cf00d69215463e5b55b974f4c657047e2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
