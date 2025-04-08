444640a79327bbc4e19d6c2047a2d9edef0c3fb2
davisherrera <daviseherrera05@gmail.com>
2025-04-08 17:11:59 -0600

    Changing back to my Davis-Branch

--- backend/Intex2.API/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
+++ backend/Intex2.API/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2579b4b7ee8d306c2c3393cdbfda8159feae8928")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43240dd2b19292268ab4d0a4d8ef3cba7d3de06a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
