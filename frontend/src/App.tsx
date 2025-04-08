1894a09f12835fe0cae62820ae06be2a5c2cf62a
davisherrera <daviseherrera05@gmail.com>
2025-04-08 09:44:16 -0600

    Trying to fix my site

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { MoviesPage } from './pages/MoviesPage'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage'
 import AdminMovieManager from './pages/test'
+import HomePage from './pages/HomePage';
+import CreateAccount from './pages/CreateAccount';
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/MoviesPage" element={<MoviesPage />} />
         <Route path="/LoginPage" element={<LoginPage />} />
+        <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/AdminMovieManager" element={<AdminMovieManager />} />
 
       </Routes>
