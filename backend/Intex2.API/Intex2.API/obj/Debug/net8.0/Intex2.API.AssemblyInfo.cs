6d21225693bd55bbf183141b03753f3e10cfa180
coltobry663 <130722636+coltobry663@users.noreply.github.com>
2025-04-09 09:08:30 -0600

    pulling

--- backend/Intex2.API/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
+++ backend/Intex2.API/Intex2.API/obj/Debug/net8.0/Intex2.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6876dd7003760a823ad364c0b742d7aeab8ee2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49e319b177aa573521cf92e0e68a7eda730a82bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
